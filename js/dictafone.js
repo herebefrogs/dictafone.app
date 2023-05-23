ac9c1104d6e2192ace7a2119b3dfdfba9f1f7e18
Jerome Lecomte <jeromelecomte@Jeromes-MacBook-Pro.local>
2023-05-23 16:32:23 -0400

    recorder and control buttons

--- js/dictafone.js
+++ js/dictafone.js
@@ -1,9 +1,43 @@
+recorder = null;
+
+function startRecording() {
+  recorder.start();
+  btn_start.style.display = 'none';
+  btn_pause.style.display = 'inline';
+  btn_stop.style.display = 'inline';
+}
+
+function stopRecording() {
+  recorder.stop();
+  btn_stop.style.display = 'none';
+  btn_pause.style.display = 'none';
+  btn_resume.style.display = 'none';
+  btn_start.style.display = 'inline';
+}
+
+function pauseRecording() {
+  recorder.pause();
+  btn_pause.style.display = 'none';
+  btn_start.style.display = 'none';
+  btn_resume.style.display = 'inline';
+  btn_stop.style.display = 'inline';
+}
+
+function resumeRecording() {
+  recorder.resume();
+  btn_resume.style.display = 'none';
+  btn_pause.style.display = 'inline';
+  btn_stop.style.display = 'inline';
+
+}
+
+
 function displayError(msg) {
   document.getElementById('error').innerText = msg;
   console.error(msg);
 }
 
-async function onload() {
+async function getAudioRecorder() {
   if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
     displayError('mediaDevices or getUserMedia() not supported on this browser.');
     return;
@@ -11,9 +45,24 @@ async function onload() {
   
   try {
     stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-
+    return new MediaRecorder(stream);
   } catch (err) {
     displayError('failed accessing micrphone due to ' + err);
+    return;
+  }
+}
+
+async function onload() {
+  recorder = await getAudioRecorder();
+
+  if (recorder) {
+    btn_start.onclick = startRecording;
+    btn_stop.onclick = stopRecording;
+    btn_pause.onclick = pauseRecording;
+    btn_resume.onclick = resumeRecording;
+    btn_stop.style.display = 'none';
+    btn_pause.style.display = 'none';
+    btn_resume.style.display = 'none';
   }
 }
 
