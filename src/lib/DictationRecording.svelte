87e677d37aded3a9d94c35a290afd5ee4de5d47c
Jerome Lecomte <jerome@herebefrogs.com>
2023-06-17 21:08:44 -0400

    Download transcript or audio recording

--- src/lib/DictationRecording.svelte
+++ src/lib/DictationRecording.svelte
@@ -1,6 +1,6 @@
 <script>
   import DictationTranscript from "./DictationTranscript.svelte";
-  import { formatDate } from "./helpers/format";
+  import { formatDate, formatTime } from "./helpers/format";
   import { recordings } from "./recordings";
 
   export let audio;
@@ -8,11 +8,25 @@
   export let date;
   export let name;
   export let transcript;
+
+  const transcript2blob = () => new Blob(transcript.map(
+    ({start_time, end_time, text}) => `${formatTime(start_time)}\n${text}\n${formatTime(end_time)}\n\n`),
+    {type: "text/plain"}
+  );
 </script>
 
 <section>
   <h2>{name}</h2>
-  <p>{formatDate(date)} <button on:click={() => recordings.delete(id)}>Delete</button></p>
+  <p>{formatDate(date)}</p>
+  <nav>
+    <button on:click={() => recordings.delete(id)}>Delete</button>
+    <button>
+      <a download={`${name}.txt`} href={URL.createObjectURL(transcript2blob())} type="text/plain">Download transcript</a>
+    </button>
+    <button>
+      <a download href={URL.createObjectURL(audio)} type={audio.type}>Download audio</a>
+    </button>
+  </nav>
   <audio src={URL.createObjectURL(audio)} controls></audio>
   <DictationTranscript transcript={transcript} />
 </section>
