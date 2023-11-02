720cf3c650f9952f37002886b1a48f3d0e9e7f9d
Jerome Lecomte <jerome@herebefrogs.com>
2023-11-02 10:24:42 -0400

    Basic routing
    Between home/list view, new transcript and transcript details

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,12 @@
 	import '../app.postcss';
 </script>
 
+<navbar class="navbar bg-base-100">
+	<div class="navbar-start"></div>
+  <div class="navbar-center">
+    <a href="/" class="btn btn-ghost normal-case text-xl">Dictafone</a>
+  </div>
+	<div class="navbar-end"></div>
+</navbar>
+
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>there will be a list of past recordings</p>
+
+
+<a href="/42" class="active">
+  <span class="btm-nav-label">Past recording #1</span>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+  </svg>
+</a>
+
+
+<div class="btm-nav">
+  <a href="/new" class="active">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M12 18.75a6 6 0 006-6v-1.5m-6 7.5a6 6 0 01-6-6v-1.5m6 7.5v3.75m-3.75 0h7.5M12 15.75a3 3 0 01-3-3V4.5a3 3 0 116 0v8.25a3 3 0 01-3 3z" />
+    </svg>
+    <span class="btm-nav-label">New transcript</span>
+  </a>
+</div>
