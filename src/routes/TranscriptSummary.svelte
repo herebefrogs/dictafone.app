7607b52804313f966688549b93b228fc91a71e25
Jerome Lecomte <jerome@herebefrogs.com>
2023-11-08 15:04:10 -0500

    Fix homepage when a saved transcript is missing the transcript text

--- src/routes/TranscriptSummary.svelte
+++ src/routes/TranscriptSummary.svelte
@@ -15,7 +15,7 @@
     <a href="/{transcript.id}">
       <div class="flex justify-between">
         <div class="truncate mr-8">
-          {transcript.transcript[0].text}
+          {transcript.transcript[0]?.text}
         </div>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
           <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
