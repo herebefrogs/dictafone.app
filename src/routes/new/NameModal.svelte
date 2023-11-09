fa9fb7a22096a0326f79b1aaee9a0469b1017a83
Jerome Lecomte <jerome@herebefrogs.com>
2023-11-09 13:21:38 -0500

    Fix Enter not saving automatically in name modal

--- src/routes/new/NameModal.svelte
+++ src/routes/new/NameModal.svelte
@@ -24,6 +24,12 @@
     }
   }
 
+  const keydown = e => {
+    if (e.key === 'Enter') {
+      save(e);
+    }
+  }
+
   const close = () => {
     emptyName = false;
     if (browser) {
@@ -35,8 +41,8 @@
 <dialog id="name_modal" class="modal" on:close={close}>
   <div class="modal-box">
     <h3 class="font-bold text-lg">Save Transcript</h3>
-    <input bind:value={$name} type="text" placeholder="Enter a transcript name" class="input input-bordered w-full max-w-xs my-4" />
- 
+    <input bind:value={$name} on:keydown={keydown} type="text" placeholder="Enter a transcript name" class="input input-bordered w-full max-w-xs my-4" />
+
     {#if emptyName}
     <div class="alert alert-error w-full max-w-xs mb-8">
       <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
