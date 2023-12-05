d696c968f1254775cb6d580ad4f00e3b5550cd14
Jerome Lecomte <jerome@herebefrogs.com>
2023-12-05 17:40:34 -0500

    Share transcript
    Use Web Share API to share the transcript text (and audio when recorded)
    via other native apps installed on the device.
    
    Also implement a new Google Analytics event "transcript_share"
    to measure feature engagement.
    
    Closes #2

--- src/routes/[id]/Actions.svelte
+++ src/routes/[id]/Actions.svelte
@@ -3,14 +3,9 @@
   import { formatTimestamp } from '$lib/helpers/format'
   import { isAndroid } from '$lib/helpers/mobile';
   import { fireEvent } from '$lib/helpers/analytics';
+  import { browser } from '$app/environment';
 
   export let transcript;
-
-  const remove = async () => {
-    await transcripts.delete(transcript.id);
-    fireEvent('transcript_delete', transcript)
-    window.location.href = '/';
-  }
 
   const transcript2blob = (lines) => new Blob(
     lines.map(({start_time, end_time, text}, i) => `${i+1}\n${formatTimestamp(start_time)} --> ${formatTimestamp(end_time)}\n${text}\n\n`),
@@ -18,7 +13,47 @@
   );
 
   const audioExt = isAndroid ? null : /audio\/(\w+)/.exec(transcript.audio.type)[1];
+  const audioFilename = `${transcript.name}.${audioExt}`;
 
+  const data = {
+    title: transcript.name,
+    files: [
+      new File(
+          [transcript2blob(transcript.lines)],
+          // this must be .txt rather than .srt to be shareable by the API
+          `${transcript.name}.txt`,
+          {type: 'text/plain'}
+        )
+    ]
+  };
+
+  // audio recording is disabled on Android
+  if (!isAndroid) {
+    data.files.push(
+      new File(
+        [transcript.audio],
+        audioFilename,
+        {type: transcript.audio.type}
+      )
+    )
+  }
+
+  const canShare = browser && navigator.canShare && navigator.canShare(data);
+
+  const share = async () => {
+    try {
+      await navigator.share(data);
+      fireEvent('transcript_share', transcript)
+    } catch (e) {
+      console.error('Web Share API error', e)
+    }
+  }
+
+  const remove = async () => {
+    await transcripts.delete(transcript.id);
+    fireEvent('transcript_delete', transcript)
+    window.location.href = '/';
+  }
 </script>
 
 
@@ -40,12 +75,20 @@
     <span class="text-base-content">Download Text</span>
   </a>
   {#if !isAndroid}
-  <a download={`${transcript.name}.${audioExt}`} href={URL.createObjectURL(transcript.audio)} type={transcript.audio.type} class="flex flex-col items-center rounded-md bg-base-200 hover:bg-base-300 py-2 w-32 download-audio">
+  <a download={audioFilename} href={URL.createObjectURL(transcript.audio)} type={transcript.audio.type} class="flex flex-col items-center rounded-md bg-base-200 hover:bg-base-300 py-2 w-32 download-audio">
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
       <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3" />
     </svg>
     <span class="text-base-content">Download Audio</span>
   </a>
+  {/if}
+  {#if canShare}
+  <button on:click={share} class="flex flex-col items-center rounded-md bg-base-200 hover:bg-base-300 py-2 w-32">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z" />
+    </svg>
+    <span class="text-base-content">Share</span>
+  </button>
   {/if}
   <button on:click={remove} class="flex flex-col items-center rounded-md bg-base-200 hover:bg-base-300 py-2 w-32">
     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
